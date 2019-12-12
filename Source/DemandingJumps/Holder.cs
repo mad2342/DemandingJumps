e7eb1b4b88258eee027f652417af89612f77c7ff
Mad <github@typ03.de>
2019-12-12 13:46:59 +0100

    fix stab pip preview

--- Source/DemandingJumps/Holder.cs
+++ Source/DemandingJumps/Holder.cs
@@ -3,5 +3,6 @@
     public static class Fields
     {
         public static bool JumpPreview = false;
+        public static bool StabilityPreview = false;
     }
 }
--- Source/DemandingJumps/Patches/AddInstability.cs
+++ Source/DemandingJumps/Patches/AddInstability.cs
@@ -1,11 +1,156 @@
 ﻿using System;
+using System.Collections.Generic;
 using BattleTech;
+using BattleTech.UI;
 using Harmony;
+using HBS;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace DemandingJumps.Patches
 {
     class AddInstability
     {
+        //UI
+        [HarmonyPatch(typeof(CombatHUDPipBar), "ShowValue")]
+        public static class CombatHUDPipBar_ShowValue_Patch
+        {
+            static void Postfix(CombatHUDPipBar __instance, float current, float projected, Color shownColor, Color shownColorProjectedHigh)
+            {
+                try
+                {
+                    if (__instance.GetComponentInParent(typeof(CombatHUDStabilityDisplay)) != null)
+                    {
+                        Logger.LogLine("[CombatHUDPipBar_ShowValue_POSTFIX] THIS CombatHUDPipBar is a child of CombatHUDStabilityDisplay");
+
+                        Logger.LogLine("[CombatHUDPipBar_ShowValue_POSTFIX] current: " + current);
+                        Logger.LogLine("[CombatHUDPipBar_ShowValue_POSTFIX] projected: " + projected);
+
+                        if (projected > current)
+                        {
+                            Logger.LogLine("[CombatHUDPipBar_ShowValue_POSTFIX] INSTABILITY RISES: Fixing pips-preview");
+
+                            float ___pointsPerPip = (float)typeof(CombatHUDPipBar).GetProperty("PointsPerPip", AccessTools.all).GetValue(__instance, null);
+                            int pipCount = __instance.PipCount;
+                            Logger.LogLine("[CombatHUDPipBar_ShowValue_POSTFIX] ___pointsPerPip: " + ___pointsPerPip);
+                            Logger.LogLine("[CombatHUDPipBar_ShowValue_POSTFIX] pipCount: " + pipCount);
+                            float calculatedMaxStability = pipCount * ___pointsPerPip;
+                            Logger.LogLine("[CombatHUDPipBar_ShowValue_POSTFIX] calculatedMaxStability: " + calculatedMaxStability);
+
+                            Color increaseStabilityWarningColor = Color.Lerp(shownColor, shownColorProjectedHigh, Mathf.Sin(Time.realtimeSinceStartup * __instance.TimeSinFactor) * 0.5f + 0.5f);
+                            Color c = LazySingletonBehavior<UIManager>.Instance.UIColorRefs.red;
+                            Color maxStabilityWarningColor = Color.Lerp(c, shownColorProjectedHigh, Mathf.Sin(Time.realtimeSinceStartup * __instance.TimeSinFactor) * 0.5f + 0.5f);
+                            Color stabilityWarningColor = projected >= calculatedMaxStability ? maxStabilityWarningColor : increaseStabilityWarningColor;
+
+
+                            List <Graphic> ___pips = (List<Graphic>)typeof(CombatHUDPipBar).GetProperty("Pips", AccessTools.all).GetValue(__instance, null);
+                            for (int i = 0; i < ___pips.Count; i++)
+                            {
+                                ___pips[i].transform.localScale = Vector3.one;
+
+                                if ( ((i+1) * ___pointsPerPip > current) && ((i + 1) * ___pointsPerPip <= projected) )
+                                {
+                                    UIHelpers.SetImageColor(___pips[i], stabilityWarningColor);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError(e);
+                }
+            }
+        }
+
+        // Reference
+        /*
+        [HarmonyPatch(typeof(CombatHUDMechTray), "Update")]
+        public static class CombatHUDMechTray_Update_Patch
+        {
+
+        static void Prefix(CombatHUDMechTray __instance)
+        {
+            try
+            {
+                if (__instance.DisplayedActor == null)
+                {
+                    return;
+                }
+
+                CombatHUDStabilityDisplay stabilityDisplay = __instance.ActorInfo.StabilityDisplay;
+                float predictedStabilityPercentage = stabilityDisplay.PredictedTargetLevel;
+                Mech mech = __instance.DisplayedActor as Mech;
+                float currentStabilityPercentage = mech.StabilityPercentage;
+
+                // Instability rises
+                if (currentStabilityPercentage < predictedStabilityPercentage)
+                {
+                    Fields.StabilityPreview = true;
+                }
+                else
+                {
+                    Fields.StabilityPreview = false;
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+            }
+        }
+
+        static void Postfix(CombatHUDMechTray __instance)
+        {
+            try
+            {
+                //AbstractActor ____actor = (AbstractActor)AccessTools.Field(typeof(CombatHUDActorInfo), "displayedActor").GetValue(__instance.ActorInfo);
+                //Logger.LogLine("[CombatHUDMechTray_Update_POSTFIX] ____actor?.DisplayName: " + ____actor?.DisplayName);
+
+
+                Logger.LogLine("[CombatHUDMechTray_Update_POSTFIX] __instance.DisplayedActor?.DisplayName: " + __instance.DisplayedActor?.DisplayName);
+
+                if(__instance.DisplayedActor == null)
+                {
+                    return;
+                }
+
+                CombatHUDStabilityDisplay stabilityDisplay = __instance.ActorInfo.StabilityDisplay;
+                float predictedStabilityPercentage = stabilityDisplay.PredictedTargetLevel;
+                Logger.LogLine("[CombatHUDMechTray_Update_POSTFIX] predictedStabilityPercentage: " + predictedStabilityPercentage);
+
+
+                Mech mech = __instance.DisplayedActor as Mech;
+                float currentStabilityPercentage = mech.StabilityPercentage;
+                Logger.LogLine("[CombatHUDMechTray_Update_POSTFIX] currentStabilityPercentage: " + currentStabilityPercentage);
+
+                // Instability rises
+                if (currentStabilityPercentage < predictedStabilityPercentage)
+                {
+                    Logger.LogLine("[CombatHUDMechTray_Update_POSTFIX] Try to fix pips");
+
+                    CombatHUDStabilityBarPips pipBar = (CombatHUDStabilityBarPips)AccessTools.Property(typeof(CombatHUDStabilityDisplay), "PipBar").GetValue(stabilityDisplay, null);
+                    List<Graphic> pipBarPips = (List<Graphic>)typeof(CombatHUDStabilityBarPips).GetProperty("Pips", AccessTools.all).GetValue(pipBar, null);
+
+                    //Vector3 fullScale = (Vector3)AccessTools.Field(typeof(CombatHUDStabilityBarPips), "fullScale").GetValue(pipBar);
+
+                    Logger.LogLine("[CombatHUDMechTray_Update_POSTFIX] pipBarPips.Count: " + pipBarPips.Count);
+
+                    for (int i = 0; i < pipBarPips.Count; i++)
+                    {
+                        pipBarPips[i].color = new Color(0, 0, 1, 0);
+                        pipBarPips[i].transform.localScale = Vector3.one;
+                    } 
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+            }
+        }
+
+        }
+        */
+
         // Info
         [HarmonyPatch(typeof(Mech), "GetMinStability", new Type[] { typeof(float), typeof(int) })]
         public static class Mech_GetMinStability_Patch
@@ -14,14 +159,14 @@ namespace DemandingJumps.Patches
             {
                 try
                 {
-                    Logger.LogLine("[Mech_GetMinStability_POSTFIX] __instance.DisplayName: " + __instance.DisplayName);
-                    Logger.LogLine("[Mech_GetMinStability_POSTFIX] oldStability: " + oldStability);
-                    Logger.LogLine("[Mech_GetMinStability_POSTFIX] numLevelsToDump: " + numLevelsToDump);
+                    //Logger.LogLine("[Mech_GetMinStability_POSTFIX] __instance.DisplayName: " + __instance.DisplayName);
+                    //Logger.LogLine("[Mech_GetMinStability_POSTFIX] oldStability: " + oldStability);
+                    //Logger.LogLine("[Mech_GetMinStability_POSTFIX] numLevelsToDump: " + numLevelsToDump);
 
                     float levelValue = __instance.MaxStability / (float)__instance.Combat.Constants.ResolutionConstants.StabilityLevels;
-                    Logger.LogLine("[Mech_GetMinStability_POSTFIX] levelValue: " + levelValue);
+                    //Logger.LogLine("[Mech_GetMinStability_POSTFIX] levelValue: " + levelValue);
 
-                    Logger.LogLine("[Mech_GetMinStability_POSTFIX] __result: " + __result);
+                    //Logger.LogLine("[Mech_GetMinStability_POSTFIX] __result: " + __result);
                 }
                 catch (Exception e)
                 {
@@ -38,7 +183,7 @@ namespace DemandingJumps.Patches
                 try
                 {
                     Logger.LogLine("[Mech_AddInstability_POSTFIX] __instance.DisplayName: " + __instance.DisplayName);
-                    Logger.LogLine("[Mech_AddInstability_POSTFIX] amt: " + amt);
+                    //Logger.LogLine("[Mech_AddInstability_POSTFIX] amt: " + amt);
                     Logger.LogLine("[Mech_AddInstability_POSTFIX] source: " + source);
 
                     if (source != StabilityChangeSource.Jumping)
@@ -80,10 +225,6 @@ namespace DemandingJumps.Patches
 
                         __instance.ApplyUnsteady();
                     }
-
-
-
-                    // @ToDo: Fix UI: CombatHUDFilledBarBase.Update
                 }
                 catch (Exception e)
                 {
