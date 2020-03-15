3c5dea9bf1e854a421cc3231706f1e5ae95c9570
Mad <github@typ03.de>
2020-03-15 09:48:52 +0100

    changed default to hit penalty to 2-2-2-2

--- Source/DemandingJumps/Settings.cs
+++ Source/DemandingJumps/Settings.cs
@@ -3,6 +3,6 @@
     internal class Settings
     {
         public bool ToHitSelfJumpedSpareAI = true;
-        public int[] ToHitSelfJumpedPenalties = new int[] { 1, 1, 2, 2};
+        public int[] ToHitSelfJumpedPenalties = new int[] { 2, 2, 2, 2};
     }
 }
