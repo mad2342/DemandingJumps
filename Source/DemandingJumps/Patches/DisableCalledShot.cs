dc0c9922546b060204adb573ca7b685f234579cf
mad <github@mad-season.de>
2021-06-27 21:28:22 +0200

    disable called shots after jumping

---
+++ Source/DemandingJumps/Patches/DisableCalledShot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BattleTech;
+using BattleTech.UI;
+using Harmony;
+
+namespace DemandingJumps.Patches
+{
+    class DisableCalledShot
+    {
+        [HarmonyPatch(typeof(CombatHUDMechwarriorTray), "ResetMechwarriorButtons")]
+        public static class CombatHUDMechwarriorTray_ResetMechwarriorButtons_Patch
+        {
+            public static void Postfix(CombatHUDMechwarriorTray __instance, AbstractActor actor)
+            {
+                try
+                {
+                    if (actor == null)
+                    {
+                        return;
+                    }
+
+                    Logger.Debug($"[CombatHUDMechwarriorTray_ResetMechwarriorButtons_POSTFIX] actor.DisplayName: {actor.DisplayName}");
+                    Logger.Debug($"[CombatHUDMechwarriorTray_ResetMechwarriorButtons_POSTFIX] actor.HasJumpedThisRound: {actor.HasJumpedThisRound}");
+                    Logger.Debug($"[CombatHUDMechwarriorTray_ResetMechwarriorButtons_POSTFIX] actor.JumpedLastRound: {actor.JumpedLastRound}");
+
+                    CombatHUDActionButton[] ___MoraleButtons = (CombatHUDActionButton[])AccessTools.Property(typeof(CombatHUDMechwarriorTray), "MoraleButtons").GetValue(__instance, null);
+
+                    if (actor.HasJumpedThisRound || actor.JumpedLastRound)
+                    {
+                        ___MoraleButtons[0].DisableButton();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+    }
+}
